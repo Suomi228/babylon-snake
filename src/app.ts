cdbc83ba4bb0341c3102e10c59d0be1abfa6d26d
Suomi228 <bixep345@gmail.com>
2024-09-11 11:59:00 +0300

    движок физики подключен

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,85 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  Mesh,
+  MeshBuilder,
+  PhysicsMotionType,
+  DistanceConstraint,
+  StandardMaterial,
+  Color3,
+  HavokPlugin,
+  PhysicsShapeType,
+  PhysicsAggregate,
+} from "@babylonjs/core";
+import HavokPhysics from "@babylonjs/havok";
+import { PhysicsBody } from "@babylonjs/core";
+
 class App {
-    constructor() {
-        // create the canvas html element and attach it to the webpage
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
+  scene: Scene;
 
-        // initialize babylon scene and engine
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
+  constructor() {
+    // create the canvas html element and attach it to the webpage
+    var canvas = document.createElement("canvas");
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.id = "gameCanvas";
+    document.body.appendChild(canvas);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+    // initialize babylon scene and engine
+    var engine = new Engine(canvas, true);
+    this.scene = new Scene(engine);
+    var camera: ArcRotateCamera = new ArcRotateCamera(
+      "Camera",
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      Vector3.Zero(),
+      this.scene
+    );
+    camera.attachControl(canvas, true);
+    var light1: HemisphericLight = new HemisphericLight(
+      "light1",
+      new Vector3(1, 1, 0),
+      this.scene
+    );
 
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'i') {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
-                } else {
-                    scene.debugLayer.show();
-                }
-            }
-        });
+    this.enablePhysic().then(() => {
+      const box1 = MeshBuilder.CreateBox("box", { width: 1, height: 1 }, this.scene);
+      const box2 = MeshBuilder.CreateBox("box", { width: 1, height: 1 }, this.scene);
+      const box3 = MeshBuilder.CreateBox("box", { width: 1, height: 1 }, this.scene);
+      const box4 = MeshBuilder.CreateBox("box", { width: 1, height: 1 }, this.scene);
+      var ground = MeshBuilder.CreateGround("ground",{ width: 10, height: 10 },this.scene);
+      box1.position = new Vector3(0, 1, 0);
+      box2.position = new Vector3(1, 1, 0);
+      box3.position = new Vector3(2, 1, 0);
+      box4.position = new Vector3(3, 1, 0);
+      let boxMaterialLightGreen = new StandardMaterial("material", this.scene);
+      boxMaterialLightGreen.emissiveColor = new Color3(0.1, 0.8, 0);
+      let boxMaterialDarkGreen = new StandardMaterial("material", this.scene);
+      boxMaterialDarkGreen.emissiveColor = new Color3(0.2, 0.48, 0.32);
+      box1.material = boxMaterialLightGreen;
+      box2.material = boxMaterialDarkGreen;
+      box3.material = boxMaterialLightGreen;
+      box4.material = boxMaterialDarkGreen;
+    });
 
-        // run the main render loop
-        engine.runRenderLoop(() => {
-            scene.render();
-        });
-    }
+    engine.runRenderLoop(() => {
+      this.scene.render();
+    });
+  }
+
+  async enablePhysic(): Promise<void> {
+    const havok = await HavokPhysics();
+    this.scene.enablePhysics(
+      new Vector3(0, -9.81, 0),
+      new HavokPlugin(true, havok)
+    );
+  }
 }
 new App();
